735b7efbc3d829f1a7434694f7e4ae3f26cb62c6
lovnishBhardwaj <76565429+lovnishBhardwaj@users.noreply.github.com>
2022-02-26 20:14:41 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,9 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+
+
+burger.addEventListener('click', ()=>{
+     navbar.classList.toggle('h-nav-resp');
+     navList.classList.toggle('v-class-resp');
+})
